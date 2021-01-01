77072df69e4c9e1f2353a2cf82a15d251bf6c219
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2021-01-01 19:49:14 -0400

    Initial commit

---
+++ TiposLayout/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TiposLayout.Grid;
+using TiposLayout.Relative;
+using TiposLayout.Scroll;
+using TiposLayout.Stack;
+using Xamarin.Forms;
+
+namespace TiposLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPaginaStack(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new StackPage());
+        }
+        private void GoPaginaGrid(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new GridPage());
+        }
+        private void GoPaginaAbsolute(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new AbsolutePage());
+        }
+        private void GoPaginaRelative(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new RelativePage());
+        }
+        private void GoPaginaScroll(Object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new ScrollPage());
+        }
+
+
+    }
+}
